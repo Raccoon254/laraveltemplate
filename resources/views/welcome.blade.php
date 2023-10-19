e5f18ac25bfe2088a89216a1407819ae636f8a6a
Steve Tom <tomsteve187@gmail.com>
2023-10-19 07:31:34 +0300

    Title welcome fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>{{ env('APP_NAME', 'RaccoonTemplate') }}</title>
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
