dc6c332e4aa4263b943dbf1284b2f12e5cb25301
Steve Tom <tomsteve187@gmail.com>
2023-10-19 07:42:53 +0300

    Welcome buttons align

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,39 +15,43 @@
 
 </head>
 <body class="antialiased bg-cover bg-center" style="background-image: url('/bg.svg');">
-
-<div
-    class="relative sm:flex sm:items-center min-h-screen bg-opacity-50 bg-gray-100 dark:bg-dots-lighter dark:bg-opacity-60 dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-
-    <!-- Check for login route -->
-    @if (Route::has('login'))
-        <div class="sm:fixed p-6 text-left z-10">
-            @auth
-                <a href="{{ url('/dashboard') }}"
-                   class="block sm:inline-block mb-2 sm:mb-0 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-            @else
-                <div class="flex gap-4">
-
-                    <a href="{{ route('login') }}">
+<section class="main flex min-h-screen items-center justify-center flex-col">
+    <div class="text-3xl">
+        Welcome message
+    </div>
+    <div class="sm:flex sm:items-center dark:bg-dots-lighter selection:bg-red-500 selection:text-white">
+        <!-- Check for login route -->
+        @if (Route::has('login'))
+            <div class="p-6 text-left z-10">
+                @auth
+                    <a href="{{ url('/dashboard') }}">
                         <x-primary-button>
-                            Login
+                            Dashboard
                         </x-primary-button>
                     </a>
+                @else
+                    <div class="flex gap-4">
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}">
-                            <x-secondary-button>
-                                Register
+                        <a href="{{ route('login') }}">
+                            <x-secondary-button class="ring-gray-50">
+                                Login
                             </x-secondary-button>
                         </a>
-                    @endif
-                </div>
-            @endauth
-        </div>
-    @endif
 
-</div>
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">
+                                <x-secondary-button>
+                                    Register
+                                </x-secondary-button>
+                            </a>
+                        @endif
+                    </div>
+                @endauth
+            </div>
+        @endif
 
+    </div>
+</section>
 </body>
 
 </html>
